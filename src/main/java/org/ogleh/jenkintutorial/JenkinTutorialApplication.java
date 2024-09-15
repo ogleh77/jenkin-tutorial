b8dc2d91907fd4d05fe9fe568501bad5c768b625
Mohamed Ogleh <mohamedogleh0@gmail.com>
2024-09-15 16:48:45 +0300

    first commit

---
+++ src/main/java/org/ogleh/jenkintutorial/JenkinTutorialApplication.java
@@ -0,0 +1,13 @@
+package org.ogleh.jenkintutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ogleh/jenkintutorial/JenkinTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ogleh.jenkintutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
